ec0dc44cc223ba5da3cf10d46d17702df88d304b
Fahad98723 <md.ravi63@gmail.com>
2022-01-28 20:20:39 +0600

    'done'

--- index.js
+++ index.js
@@ -164,6 +164,12 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/blog/comments/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id : ObjectId(id)}
+            const result = await commentsCollection.deleteOne(query)
+            res.json(result)
+        })
 
     }
     finally {
